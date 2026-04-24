d4f990b5d6372d35e0447874180b14618f28277a
milijanic <martina.ilijanic@elixirr.com>
2026-04-24 11:14:55 +0200

    Update source_not_in_target__DimSupplier.sql

--- dbt_project/drip_transformations/tests/source_not_in_target__DimSupplier.sql
+++ dbt_project/drip_transformations/tests/source_not_in_target__DimSupplier.sql
@@ -1,14 +1,14 @@
 -- Singular dbt test: SourceNotInTarget for DimSupplier
 --
--- Fails if any business keys present in the source view (vw_DimSupplier)
+-- Fails if any primary-key hashes present in the source view (vw_DimSupplier)
 -- are missing from the target table (DimSupplier).
 --
 -- Run locally in dev with:
 --   dbt test --select source_not_in_target__DimSupplier --target dev
 
 SELECT
-    src.SupplierHashBK
+    src.SupplierHashPK
 FROM {{ ref('vw_DimSupplier') }} AS src
 LEFT JOIN {{ ref('DimSupplier') }} AS tgt
-    ON src.SupplierHashBK = tgt.SupplierHashBK
-WHERE tgt.SupplierHashBK IS NULL
+    ON src.SupplierHashPK = tgt.SupplierHashPK
+WHERE tgt.SupplierHashPK IS NULL
